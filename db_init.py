bb2c035884d2c45056dc6b5c0c3fad715dab897f
Lance Zeligman <lance.zeligman@sestrasystems.com>
2019-10-14 05:35:58 -0400

    Got the database feature working

--- db_init.py
+++ db_init.py
@@ -2,21 +2,6 @@ import logging
 
 from pymongo import MongoClient
 
-logger = logging.getLogger("zz-8")
-logger.setLevel(logging.INFO)
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler("zz-8.log")
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter("%(asctime)s %(levelname)8s %(message)s")
-
-c_handler.setFormatter(formatter)
-f_handler.setFormatter(formatter)
-
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
-
 
 class zz8_db(object):
     """
@@ -56,7 +41,7 @@ class zz8_db(object):
             f"@{self.host}:{self.port}/zz8",
         )
         self.client = MongoClient(connect_string)
-        self.logger.INFO("Connected to mongo")
+        self.logger.info("Connected to mongo")
         return True
 
     def db_init(self):
@@ -66,8 +51,8 @@ class zz8_db(object):
 
         Returns True
         """
-        self.db = self.client["zz8"]
-        self.logger.INFO("Connected to zz8 db")
+        self.db = self.client.zz8
+        self.logger.info("Connected to zz8 db")
         return True
 
     def get_user_interests(self, user_id):
@@ -82,16 +67,15 @@ class zz8_db(object):
         """
         # Creates a representation
         # of the mongo db
-        users = self.db.users
 
         # Sets up the query dict
-        query = {"user_id": user_id}
+        query = {"uuid": user_id}
 
         # Queries for that user
         # then pulls only interests
         # out of the resulting dict
-        interests = users.find_one(query)["interests"]
-        self.logger.INFO("Returning interests list")
+        interests = self.db.users.find_one(query)["interests"]
+        self.logger.info("Returning interests list")
 
         return interests
 
@@ -109,9 +93,9 @@ class zz8_db(object):
 
         # Creates a representation
         # of the mongo db
-        users = self.db.users
+        self.db.users
         doc = {"uuid": user_id, "interests": interests}
 
-        user_id = users.insert_one(doc).inserted_id
+        self.db.users.insert_one(doc).inserted_id
 
         return True
