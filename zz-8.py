9f05ef40fb17d05fd506669fe00a698d412c8e73
Lance Zeligman <lance.zeligman@sestrasystems.com>
2019-10-14 05:35:58 -0400

    Got the database feature working

--- zz-8.py
+++ zz-8.py
@@ -7,11 +7,27 @@ Includes some intregation with reddit
 """
 import configparser
 import os
+import logging
 import re
 import sys
 from db_init import zz8_db
 import praw
 from discord.ext import commands
+
+logger = logging.getLogger("zz-8")
+logger.setLevel(logging.INFO)
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler("zz-8.log")
+c_handler.setLevel(logging.INFO)
+f_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s %(levelname)8s %(message)s")
+
+c_handler.setFormatter(formatter)
+f_handler.setFormatter(formatter)
+
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
 
 
 def getconfigpath():
@@ -39,8 +55,8 @@ def readconfig():
 
     appconfig = {}
     appconfig["client_token"] = config.get("prod", "client_token")
-    appconfig["reddit_client_id"] = config.get("prod", "reddit_client_id")
-    appconfig["reddit_client_secret"] = config.get("prod", "reddit_client_secret")
+    appconfig["reddit_cid"] = config.get("prod", "reddit_client_id")
+    appconfig["reddit_csec"] = config.get("prod", "reddit_client_secret")
     appconfig["db_user"] = config.get("prod", "db_user")
     appconfig["db_pass"] = config.get("prod", "db_pass")
     appconfig["db_host"] = config.get("prod", "db_host")
@@ -54,8 +70,8 @@ config = readconfig()
 # Sets up the reddit instance
 # for querying subreddits
 reddit = praw.Reddit(
-    client_id=config["reddit_client_id"],
-    client_secret=config["reddit_client_secret"],
+    client_id=config["reddit_cid"],
+    client_secret=config["reddit_csec"],
     user_agent="zz-8",
 )
 
@@ -63,68 +79,100 @@ TEST_RE = re.compile(r"(?i)ye{2,}t")
 YEET_URL = "https://www.youtube.com/watch?v=2Bjy5YQ5xPc"
 DESCRIPTION = "ZZ-8 the lovable youngest bot"
 
-ZZ8 = commands.Bot(command_prefix="!", description=DESCRIPTION)
-
+bot = commands.Bot(command_prefix="!", description=DESCRIPTION)
 zz8_db = zz8_db(config)
 zz8_db.connection()
 zz8_db.db_init()
 
 
-def reddit_posts(subreddit, posts):
-    """
-    Function to search subreddits for new posts
-    """
+class Interests(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    return reddit.subreddit(subreddit).new(limit=posts)
+    @commands.command()
+    async def list_interests(self, ctx):
+        """
+        Command to add user interests
+        """
+        uuid = ctx.message.author.id
+        interests = []
+
+        response = zz8_db.get_user_interests(uuid)
+
+        for var in response:
+            interests.append(var.lower())
+
+        logger.info(f"retrieved interests for user {uuid}")
+        await ctx.send(f"According to what I've been",
+                       f"told, you like {interests}",)
+
+    @commands.command()
+    async def add_interests(self, ctx, *topic):
+        """
+        Command to add user interests
+        """
+        uuid = ctx.message.author.id
+        interests = []
+
+        for var in topic:
+            interests.append(var.lower())
+
+        zz8_db.store_user_interests(uuid, interests)
+        logger.info(f"Stored interests for user {uuid}")
+        await ctx.send(f"Thank you for letting me know you like {topic}")
+
+    @commands.command()
+    async def quick_reddit_view(self, ctx):
+        """
+        Do a quick feed for reddit
+
+        Posts top 5 posts from a users list interests
+        """
+        uuid = ctx.message.author.id
+        message = []
+        response = zz8_db.get_user_interests(uuid)
+
+        for sub in reddit_posts(response, 5):
+            message.append(sub.title)
+            message.append(f'<https://reddit.com{sub.permalink}>')
+
+        await ctx.send('\n'.join(message))
+
+    @commands.command()
+    async def blarg(ctx):
+        """
+        Basic command just to get back into the swing of it
+        """
+        await ctx.send("blarblarg")
+
+    @commands.command()
+    async def reddit(ctx, subreddit, posts):
+        """
+        Reddit command for zz-8 for a user
+        specified subreddit and number of posts
+        """
+
+        for sub in reddit_posts(subreddit, int(posts)):
+            await ctx.send(sub.title)
+            await ctx.send(sub.url)
 
 
-@ZZ8.event
+@bot.event
 async def on_ready():
     """
     Gives us some status output for zz-8
     """
-    print(f"Logged on as {ZZ8.user.name}, beep beep")
+    print(f"Logged on as {bot.user.name}, beep beep")
 
 
-@ZZ8.command()
-async def add_interests(ctx, topic):
-    """
-    Command to add user interests
-    """
-    interests = []
-    interests.append(topic.lower())
-    zz8_db.store_user_interests(interests)
-
-    await ctx.send(f"Thank you for letting me know you like {topic}")
-
-
-@ZZ8.command()
-async def blarg(ctx):
-    """
-    Basic command just to get back into the swing of it
-    """
-    await ctx.send("blarblarg")
-
-
-@ZZ8.command()
-async def reddit(ctx, subreddit, posts):
-    """
-    Reddit command for zz-8 for a user specified subreddit and number of posts
-    """
-
-    for sub in reddit_posts(subreddit, int(posts)):
-        await ctx.send(sub.title)
-        await ctx.send(sub.url)
-
-
-@ZZ8.event
+@bot.event
 async def on_message(message):
     """
     This is the real action for zz-8
     """
     # No self replies for the bot
 
-    if message.author == ZZ8.user:
+    if message.author == bot.user:
         return
 
     if message.content == "Hello":
@@ -138,7 +186,22 @@ async def on_message(message):
             await message.channel.send(sub.title)
             await message.channel.send(sub.url)
 
-    await ZZ8.process_commands(message)
+    await bot.process_commands(message)
 
 
-ZZ8.run(config["client_token"])
+def reddit_posts(subreddit, num_posts):
+    """
+    Function to search subreddits for new posts
+    """
+    limit = len(subreddit) * num_posts
+    sub_list = "+".join(subreddit)
+    posts = []
+
+    for sub in reddit.subreddit(sub_list).new(limit=limit):
+        posts.append(sub)
+
+    return posts
+
+
+bot.add_cog(Interests(bot))
+bot.run(config["client_token"])
