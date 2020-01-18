8ab99060d1f1589a2cec6595599d179c0969e3aa
Lance Zeligman <lance.zeligman@sestrasystems.com>
2020-01-18 00:05:03 -0500

    This should finally be finished

--- cogs/admin.py
+++ cogs/admin.py
@@ -21,10 +21,10 @@ class Admin(commands.Cog):
         :type ctx: Object
         """
 
-        ignore_channels = []
+        ignored_channels = []
         author = ctx.message.author
         channel = ctx.channel
-        guild = ctx.message.guild
+        guild = ctx.message.guild.id
         channels = ctx.message.guild.text_channels
 
         def check(m):
@@ -37,10 +37,18 @@ class Admin(commands.Cog):
         table.add_column(column_names[0], numbers)
         table.add_column(column_names[1], channel_names)
         await ctx.send("What channels should I not interact with?")
+        asyncio.sleep(2)
         await ctx.send(f"```{table}```")
         await ctx.send("Please reply with a space separated list of numbers")
         res = await self.bot.wait_for("message", check=check, timeout=180)
         ignores = res.content.split(" ")
         for i in ignores:
-            ignore_channels.append({"name": channels[i].name, "id": channels[i].id})
-
+            ignored_channels.append(channels[int(i) - 1].id)
+        prefs = self.zz8_db.get_guild_prefs(guild)
+        if not prefs:
+            self.zz8_db.store_guild_prefs(guild, ignored_channels)
+            self.bot.guilds[guild] = ignored_channels
+            self.logger.info(f"Stored preferences for guild {guild}")
+        else:
+            self.bot.guilds[guild].append(ignored_channels)
+            self.zz8_db.update_guild_prefs(guild, self.bot.guilds[guild][ignored_channels])
