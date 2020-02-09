706f75d2db02ddd539ca0d8cf286f99f224ec409
Lance Zeligman <lance.zeligman@sestrasystems.com>
2020-02-09 15:21:19 -0500

    Ignorable channels now works

---
+++ cogs/events.py
@@ -0,0 +1,64 @@
+import asyncio
+import discord
+import re
+from discord.ext import commands
+from discord.utils import get
+
+
+TEST_RE = re.compile(r"(?i)ye{2,}t")
+YEET_URL = "https://www.youtube.com/watch?v=2Bjy5YQ5xPc"
+
+
+class Events(commands.Cog):
+    def __init__(self, bot, logger):
+        self.bot = bot
+        self.logger = logger
+        self.guilds = []
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """
+        on_ready When the bot starts up, executes these functions
+        """
+        self.guilds = self.bot.get_cog("Admin").guilds
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        """
+        This is the real action for zz-8
+        """
+        # No self replies for the bot
+
+        #    channel = message.channel
+        #    guild = message.guild
+        #    if channels[guild][channel]:
+        #        return
+        if message.channel.id in self.guilds:
+            return
+
+        if message.author == self.bot.user:
+            return
+
+        if message.content == "Hello":
+            await message.channel.send("World")
+
+        if TEST_RE.search(message.content):
+            await message.channel.send(YEET_URL)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, old_msg, new_msg):
+        """
+        on_message_edit Performs actions on message edits
+
+        :param old_msg: Details of the message before edit
+        :type old_msg: Ctx
+        :param new_msg: Details of the message after edit
+        :type new_msg: Ctx
+        """
+        emoji = get(self.bot.emojis, name="rah")
+
+        if new_msg.channel.id in self.guilds:
+            return
+
+        if new_msg.author != self.bot.user:
+            await new_msg.add_reaction(emoji)
--- zz-8.py
+++ zz-8.py
@@ -17,10 +17,11 @@ from datetime import datetime
 from discord.ext import commands, tasks
 from discord.utils import get
 from db_init import zz8_db
+from cogs.admin import Admin
 from cogs.interests import Interests
 from cogs.music import Music
 from cogs.reminders import Reminders
-from cogs.admin import Admin
+from cogs.events import Events
 
 logger = logging.getLogger("zz-8")
 logger.setLevel(logging.INFO)
@@ -79,8 +80,6 @@ reddit = praw.Reddit(
     user_agent="zz-8",
 )
 
-TEST_RE = re.compile(r"(?i)ye{2,}t")
-YEET_URL = "https://www.youtube.com/watch?v=2Bjy5YQ5xPc"
 DESCRIPTION = "ZZ-8 the lovable youngest bot"
 
 zz8_db = zz8_db(config)
@@ -89,86 +88,10 @@ zz8_db.db_init()
 
 bot = commands.Bot(command_prefix="!", description=DESCRIPTION)
 
-
-class bot_shit(object):
-    guilds = zz8_db.get_channel_prefs()
-
-
-zz8 = bot_shit()
-
-
-@bot.event
-async def on_ready():
-    """
-    on_ready Sets up important variables for zz8
-    """
-
-    # zz8.channels = zz8_db.load_channels
-    print(f"Logged on as {bot.user.name}, beep beep")
-
-
-@bot.event
-async def on_message_edit(old_msg, new_msg):
-    """
-    Performs a list of actions on reciving message
-    edit event
-    """
-    if (
-        zz8.guilds
-        and new_msg.channel.id in zz8.guilds[new_msg.guild.id]["ignored_channels"]
-    ):
-        return
-
-    emoji = get(bot.emojis, name="rah")
-    if new_msg.author != bot.user:
-        await new_msg.add_reaction(emoji)
-
-
-@bot.event
-async def on_message_delete(message):
-    if (
-        zz8.guilds
-        and message.channel.id in zz8.guilds[message.guild.id]["ignored_channels"]
-    ):
-        return
-
-    uuid = message.author.id
-    if message.author != bot.user:
-        await message.channel.send(f"I saw that, <@{uuid}>")
-
-
-@bot.event
-async def on_message(message):
-    """
-    This is the real action for zz-8
-    """
-    # No self replies for the bot
-
-    #    channel = message.channel
-    #    guild = message.guild
-    #    if channels[guild][channel]:
-    #        return
-    if (
-        zz8.guilds
-        and message.channel.id in zz8.guilds[message.guild.id]["ignored_channels"]
-    ):
-        return
-
-    if message.author == bot.user:
-        return
-
-    if message.content == "Hello":
-        await message.channel.send("World")
-
-    if TEST_RE.search(message.content):
-        await message.channel.send(YEET_URL)
-
-    await bot.process_commands(message)
-
-
+bot.add_cog(Admin(bot, zz8_db, logger))
 bot.add_cog(Music(bot))
 bot.add_cog(Interests(bot, zz8_db, reddit, logger))
 bot.add_cog(Reminders(bot, zz8_db, logger))
-bot.add_cog(Admin(bot, zz8_db, zz8, logger))
+bot.add_cog(Events(bot, logger))
 bot.run(config["client_token"])
 
